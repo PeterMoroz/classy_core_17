a76443ef26c8046c9611c50fc7a2698777d37f3b
Peter Moroz <ptr.moroz@gmail.com>
2022-05-01 21:19:09 +0300

    refactoring ProgramMemory module

--- part3_b/ProgramMemory.v
+++ part3_b/ProgramMemory.v
@@ -1,43 +1,28 @@
 module ProgramMemory
-  (
-    input wire i_clk,
-    input wire[15:0] i_addr,
-    output reg[15:0] o_data
-  );
-  
-  /*
-  reg[15:0] cells[7:0];
-  
-  cells[7] = 16'h0000; // unused
-  cells[6] = 16'h0000; // unused
-  cells[5] = 16'h0000; // unused
-  cells[4] = 16'h9409; // ijmp
-  cells[3] = 16'hcfff; // rjmp .-2
-  cells[2] = 16'hcfff; // rjmp .-2
-  cells[1] = 16'hfe00; // sbrs r0, 0  
-  cells[0] = 16'hc003; // rjmp .+6
-  
-  always @(posedge i_clk)
-    begin
-      if (i_clk == 1'b1)
-        o_data = cells[i_addr];
-    end
-    */
-  
-  always @(posedge i_clk)
-    begin
-      if (i_clk == 1'b1)
-        case(i_addr)
-          16'h0000: o_data = 16'hc003; // rjmp .+6
-          16'h0001: o_data = 16'hfe00; // sbrs r0, 0
-          16'h0002,
-          16'h0003: o_data = 16'hcfff; // rjmp .-2		  
-          16'h0004: o_data = 16'h9409; // ijmp
-          16'h0005,
-          16'h0006,
-          16'h0007: o_data = 16'h0000;
-          default:  o_data = 16'h0000;
-        endcase
-    end  
-  
+(
+input wire i_clk,
+input wire[15:0] i_addr,
+output reg[15:0] o_data
+);
+
+reg[15:0] memory_cells[7:0];
+
+initial
+begin
+	memory_cells[0] = 16'hc003; // rjmp .+6
+	memory_cells[1] = 16'hfe00; // sbrs r0, 0
+	memory_cells[2] = 16'hcfff; // rjmp .-2
+	memory_cells[3] = 16'hcfff; // rjmp .-2
+	memory_cells[4] = 16'h9409; // ijmp
+	memory_cells[5] = 16'h0000;
+	memory_cells[6] = 16'h0000;
+	memory_cells[7] = 16'h0000;
+end
+
+always @(posedge i_clk)
+begin
+  if (i_clk == 1'b1)
+  	o_data <= memory_cells[i_addr];
+end
+
 endmodule
