9607887ae1a7e452159562dd5cf6449f421bcd8c
Peter Moroz <ptr.moroz@gmail.com>
2022-06-05 09:26:39 +0300

    rename a couple of variables to make code a bit more clear

--- part3_b/top.v
+++ part3_b/top.v
@@ -18,14 +18,14 @@ wire[1:0] mode12K, modeAddZA;
 wire modePCZ;
 wire loadPC, loadIR;
 
-wire[15:0] rg_IR, rg_K;
+wire[15:0] IR, K;
 
 
 CtrlFetch ctrl_fetch_0(.i_clk(i_clk), .i_reset(reset), 
 	// control path
 	.i_mode12K(mode12K), .i_modeAddZA(modeAddZA), .i_modePCZ(modePCZ), .i_loadIR(loadIR), .i_loadPC(loadPC), 
 	// data path
-	.i_K(rg_K), .i_A(i_A), .i_Z(i_Z), .o_IR(rg_IR), .o_PMDATA(o_PMDATA),
+	.i_K(K), .i_A(i_A), .i_Z(i_Z), .o_IR(IR), .o_PMDATA(o_PMDATA),
 	// memory interface
 	.i_PMDATA(pm_data), .o_PMADDR(pm_addr)
 	);
@@ -34,11 +34,11 @@ ProgramMemory program_memory_0(.i_clk(i_clk), .i_addr(pm_addr), .o_data(pm_data)
 
 ControlUnit control_unit_0(.i_clk(i_clk), .i_reset(i_reset_ext),
 	// FSM input
-	.i_IR(rg_IR), .i_ALU_Z(i_ALU_Z),
+	.i_IR(IR), .i_ALU_Z(i_ALU_Z),
 	// control path outputts
 	.o_reset(reset), .o_mode12K(mode12K), .o_modeAddZA(modeAddZA), .o_modePCZ(modePCZ), .o_loadPC(loadPC), .o_loadIR(loadIR), 
 	// data path
-	.o_K(rg_K)
+	.o_K(K)
 	);
 
 endmodule
